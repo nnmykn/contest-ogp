365eb13625eb9a45335b6efb531bc2af6997f126
Kan Ninomiya <ninomiyakan.dev@gmail.com>
2023-08-18 00:22:20 +0900

    no font

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -4,11 +4,7 @@ export const config = {
   runtime: "experimental-edge",
 };
 
-export default async function handler() {
-  const fontData = await fetch(
-    new URL("../../assets/KosugiMaru-Regular.ttf", import.meta.url),
-  ).then((res) => res.arrayBuffer());
-
+export default function handler() {
   return new ImageResponse(
     (
       <div
@@ -19,23 +15,11 @@ export default async function handler() {
           width: "100%",
           height: "100%",
           backgroundColor: "white",
-          fontFamily: '"Roboto"',
-          fontWeight: "bold",
           fontSize: "128px",
         }}
       >
         Hello world!
       </div>
     ),
-    {
-      fonts: [
-        {
-          name: "Roboto",
-          data: fontData,
-          weight: 700,
-          style: "normal",
-        },
-      ],
-    },
   );
 }
