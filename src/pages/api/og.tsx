b3458d75bac3e4ba7d80356c5340b1fd74a994e5
Kan Ninomiya <ninomiyakan.dev@gmail.com>
2023-08-18 01:33:16 +0900

    https

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -4,7 +4,7 @@ export const config = {
   runtime: "edge",
 };
 
-const font = fetch(new URL("../../assets/KosugiMaru.ttf", import.meta.url)).then((res) =>
+const font = fetch(new URL("https://contest-ogp.vercel.app/KosugiMaru.ttf", import.meta.url)).then((res) =>
   res.arrayBuffer(),
 );
 
