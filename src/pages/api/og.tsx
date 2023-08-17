21fdc1052d17bb6389515bfcff9abd89db831b9f
Kan Ninomiya <ninomiyakan.dev@gmail.com>
2023-08-18 00:41:48 +0900

    edge

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -1,25 +1,29 @@
 import { ImageResponse } from "@vercel/og";
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 export const config = {
-  runtime: "experimental-edge",
+    runtime: "edge",
 };
 
-export default function handler() {
-  return new ImageResponse(
-    (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100%",
-          height: "100%",
-          backgroundColor: "black",
-          fontSize: "128px",
-        }}
-      >
-        Hello world!
-      </div>
-    ),
-  );
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+    const title = req.query.title || 'ぼくらの';
+    console.log(res)
+
+    return new ImageResponse(
+        (
+            <div
+                style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "white",
+                    fontSize: "128px",
+                }}
+            >
+                {title}
+            </div>
+        ),
+    );
 }
