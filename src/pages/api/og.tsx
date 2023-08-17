7328fe8bfc312bae8f72819a240a58eb4ea09ba7
Kan Ninomiya <ninomiyakan.dev@gmail.com>
2023-08-18 00:26:51 +0900

    black

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -14,7 +14,7 @@ export default function handler() {
           alignItems: "center",
           width: "100%",
           height: "100%",
-          backgroundColor: "white",
+          backgroundColor: "black",
           fontSize: "128px",
         }}
       >
